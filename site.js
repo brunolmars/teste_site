ea459a21e642ea7efbe5c0dde2a690664d420ddb
brunolmars <126252696+brunolmars@users.noreply.github.com>
2024-06-20 22:42:27 -0300

    Create site.js

---
+++ site.js
@@ -0,0 +1,35 @@
+Vue.component('game-list', {
+    props: ['games'],
+    template: `
+        <div class="games">
+            <game-item v-for="(game, index) in games" :key="index" :game="game"></game-item>
+        </div>
+    `
+});
+
+Vue.component('game-item', {
+    props: ['game'],
+    template: `
+        <a :href="'#'" class="game-link" @click="openGame(game.nome)">
+            {{ game.nome }}
+        </a>
+    `,
+    methods: {
+        openGame(gameName) {
+            // lógica para abrir o jogo específico
+            alert(`Abrindo ${gameName}...`);
+            
+        }
+    }
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        games: [
+            { nome: 'Jogo 1', descricao: 'Descrição do Jogo 1', preco: 'R$ 50,00', nota: 4.5 },
+            { nome: 'Jogo 2', descricao: 'Descrição do Jogo 2', preco: 'R$ 40,00', nota: 4.2 },
+            { nome: 'Jogo 3', descricao: 'Descrição do Jogo 3', preco: 'R$ 60,00', nota: 4.8 }
+        ]
+    }
+});
